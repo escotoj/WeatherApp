2250baa5e2bb020785f1c08d3a68ca36d78f787b
escotoj <j.escoto14@gmail.com>
2023-03-20 17:55:02 -0700

    added final touches and details to current and forecast

--- app.js
+++ app.js
@@ -1,56 +1,121 @@
 // need to grab value of input box and tie a function to the button to make the a call
 
-var inputBox = document.querySelector("#username")
+var inputBox = document.querySelector("#username");
 
-var inputButton = document.querySelector("#searcBtn")
+var inputButton = document.querySelector("#searcBtn");
 
-var inputForm = document.querySelector("#user-form") 
+var inputForm = document.querySelector("#user-form");
 
-var todaysForecast = document.querySelector("#todaysForecast") 
+var todaysForecast = document.querySelector("#todaysForecast");
 
+var forecast = document.querySelectorAll(".forecast");
 
 // need to get the input value to bring up data from api, SEE BELOW
 // var cityName = inputBox.value.trim()
 
 function weatherSearch(event) {
-    event.preventDefault();
-    console.log(inputBox.value.trim())
-var APIKey = "0ed67e384b6b6eefec8f0b720d30c576"
-// var lat = "44.34"
-// var lon = "10.99"
-var cityAPI = `https://api.openweathermap.org/data/2.5/forecast?q=${inputBox.value}&appid=${APIKey}`
-console.log("city value", inputBox.value)
+  event.preventDefault();
+  //   console.log(inputBox.value.trim());
+  var APIKey = "0ed67e384b6b6eefec8f0b720d30c576";
+  // var lat = "44.34"
+  // var lon = "10.99"
+  var cityAPI = `https://api.openweathermap.org/data/2.5/forecast?q=${inputBox.value}&appid=${APIKey}`;
+  console.log("city value", inputBox.value);
 
+  fetch(cityAPI)
+    .then((res) => res.json())
+    .then((data) => {
+      //   console.log(data);
+      const lat = data.city.coord.lat;
+      const lon = data.city.coord.lon;
 
-    fetch(cityAPI)
-      .then(res => res.json())
-      .then(data => {
-        console.log(data)
-        const lat = data.city.coord.lat
-        const lon = data.city.coord.lon
-        
-        var valTemp = data.list[0].main.temp
-        var temp = Math.round(((valTemp-273.15)*1.8)+32)
-        var humi = data.list[0].main.humidity
-        var date = data.list[0]
+      var valTemp = data.list[0].main.temp;
+      var temp = Math.round((valTemp - 273.15) * 1.8 + 32);
+      var humi = data.list[0].main.humidity;
+      var date = data.list[0];
+      var name = data.city.name;
 
-        todaysForecast.textContent = temp
-        todaysForecast.textContent = humi
-        // create HTML element to a vari and text content like above 
-        // in the information there is a clear day icon that i must have rendered
+      var cityWind = data.list[0].wind.speed;
+      const currentDate = dayjs().format("dddd, h:mA MM-DD-YYYY");
 
+      // doing a double search and want breaks in between
 
-        var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`
-        fetch(apiUrl)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            console.log("data.list", date)    
-        })
-      })}
+      todaysForecast.textContent = [
+        "City: " +
+          name +
+          "\n" +
+          "  Current Temp: " +
+          temp +
+          "  Humidity:" +
+          humi +
+          "  Wind :" +
+          cityWind +
+          " mile/h" +
+          " Date & Time: " +
+          currentDate,
+      ];
+      //   var todaysImg = document.createElement("img");
+      //         todaysImg.setAttribute(
+      //           "src",
+      //           `https://openweathermap.org/img/wn/${data.list[index].weather[0].icon}@2x.png`
+      //         );
 
+      // create HTML element to a vari and text content like above
+      // in the information there is a clear day icon that i must have rendered
+
+      var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}&units=imperial`;
+      fetch(apiUrl)
+        .then((res) => res.json())
+        .then((data) => {
+          //   console.log(data);
+          console.log("data.list", date);
+          for (var i = 0; i < forecast.length; i++) {
+            forecast[i].innerHTML = "";
+            var index = i * 8 + 4;
+            var forecastImg = document.createElement("img");
+            forecastImg.setAttribute(
+              "src",
+              `https://openweathermap.org/img/wn/${data.list[index].weather[0].icon}@2x.png`
+            );
+            var forecastTemp = document.createElement("p");
+            forecastTemp.innerHTML =
+              "Temp: " + Math.floor(data.list[index].main.temp) + "&#176F";
+            var forecastWind = document.createElement("p");
+            forecastWind.innerHTML =
+              "Humidity: " + data.list[index].main.humidity;
+            var forecastCity = document.createElement("p");
+            forecastCity.innerHTML = "City: " + data.city.name;
+            var forecastDate = document.createElement("p");
+            forecastDate.innerHTML = "Date: " + dayjs().format("dddd, h:mA MM-DD-YYYY");
+
+            forecast[i].append(forecastImg);
+            forecast[i].append(forecastTemp);
+            forecast[i].append(forecastWind);
+            forecast[i].append(forecastCity);
+            forecast[i].append(forecastDate);
+
+          }
+        });
+    });
+}
 
 // do a city search function for buttons
+var cityArr = [];
+inputForm.addEventListener("submit", (e) => {
+  weatherSearch(e);
+  cityArr.push(inputBox.value.trim());
+  localStorage.setItem("city", JSON.stringify(cityArr));
+  renderSearches();
+});
 
-inputForm.addEventListener("submit", (e) => weatherSearch(e))
+var searchedCities = JSON.parse(localStorage.getItem("city"));
 
+function renderSearches() {
+  var cityLi = document.createElement("li");
+  cityLi.innerHTML = inputBox.value.trim();
+  inputForm.append(cityLi);
+  cityLi.addEventListener("click", function () {
+    var searchValue = cityLi.innerHTML;
+    weatherSearch(searchValue);
+  });
+}
